8f8bab8bf910c9e37d77e43a56325bf87033029a
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-03-12 15:40:39 +0100

    route to delete experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -194,7 +194,7 @@ router.put(
 
       profile.experience.unshift(newExp);
 
-      await profile.save;
+      await profile.save();
 
       res.json(profile);
     } catch (error) {
@@ -203,4 +203,26 @@ router.put(
     }
   }
 );
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    //Get index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
 module.exports = router;
