c40cdccd085c49fc86db9db3b6e57a3971b84ca0
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-03-13 09:37:14 +0100

    route to dislike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -104,10 +104,10 @@ router.delete("/:id", auth, async (req, res) => {
 // @route   PUT api/posts/like/:id
 // @desc    Like a post
 // @access  Private
-router.put("/likes/:id", auth, async (req, res) => {
+router.put("/like/:id", auth, async (req, res) => {
   try {
     post = await Post.findById(req.params.id);
-    
+
     //Check if the post been liked by the user
     if (
       post.likes.filter((like) => like.user.toString() == req.user.id).length >
@@ -118,6 +118,36 @@ router.put("/likes/:id", auth, async (req, res) => {
 
     post.likes.unshift({ user: req.user.id });
 
+    await post.save();
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Unlike a post
+// @access  Private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    post = await Post.findById(req.params.id);
+
+    //Check if the post been liked by the user
+    if (
+      post.likes.filter((like) => like.user.toString() == req.user.id)
+        .length === 0
+    ) {
+      return res
+        .status(400)
+        .json({ msg: "You need to like a post to unlike it" });
+    }
+    //Get remove index
+    const removeIndex = post.likes.map((likes) =>
+      likes.user.toString().indexOf(req.user.id)
+    );
+    //Remove a like
+    post.likes.splice(removeIndex, 1);
     await post.save();
     res.json(post.likes);
   } catch (error) {
