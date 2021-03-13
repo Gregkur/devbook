a1a623b277bf0c14abc40d17c6177b7ce8f78bd3
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-03-13 14:52:57 +0100

    redux boilerplace setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Navbar from "./componenets/layout/Navbar";
 import Landing from "./componenets/layout/Landing";
 import Login from "./componenets/auth/Login";
 import Register from "./componenets/auth/Register";
+
 import "./App.css";
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
