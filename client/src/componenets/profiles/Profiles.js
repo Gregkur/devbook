f2210fcf87377f6cefade989ddb5dddba1ea2036
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-03-31 17:52:45 +0200

    added profiles to display

--- client/src/componenets/profiles/Profiles.js
+++ client/src/componenets/profiles/Profiles.js
@@ -2,13 +2,35 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
-import {getProfiles} from "../../actions/profile";
+import ProfileItem from "./ProfileItem";
+import { getProfiles } from "../../actions/profile";
 
 const Profiles = ({ profile: { profiles, loading }, getProfiles }) => {
   useEffect(() => {
     getProfiles();
   }, []);
-  return <div></div>;
+  return (
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <h1 className="large text-primary">Developers</h1>
+          <p className="lead">
+            <i className="fab fa-connectdevelop"></i> Browse and connect with
+            developers.
+          </p>
+          <div className="profiles">
+              {profiles.length > 0? (
+                  profiles.map(profile =>(
+                      <ProfileItem key={profile._id} profile={profile}/>
+                  ))
+              ) : <h4>No profiles found</h4>}
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 Profiles.propTypes = {
