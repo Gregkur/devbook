4b731dcba4e6de051b7803f8cf43c25444a39e5c
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-04-01 13:58:31 +0200

    changing the styling

--- client/src/componenets/profile/ProfileAbout.js
+++ client/src/componenets/profile/ProfileAbout.js
@@ -9,21 +9,21 @@ const ProfileAbout = ({
   },
 }) => {
   const mappedSkills = skills.map((skill, index) => (
-    <div class="p-1" key={index}>
-      <i class="fa fa-check"></i> {skill}
+    <div className="p-1" key={index}>
+      <i className="fa fa-check"></i> {skill}
     </div>
   ));
   return (
-    <div class="profile-about bg-light p-2">
+    <div className="profile-about bg-light p-2">
       {bio && (
         <>
-          <h2 class="text-primary">{name.trim().split(" ")[0]}'s Bio</h2>
+          <h2 className="text-primary">{name.trim().split(" ")[0]}'s Bio</h2>
           <p>{bio}</p>
-          <div class="line"></div>
+          <div className="line"></div>
         </>
       )}
-      <h2 class="text-primary">Skill Set</h2>
-      <div class="skills">{mappedSkills}</div>
+      <h2 className="text-primary">Skill Set</h2>
+      <div className="skills">{mappedSkills}</div>
     </div>
   );
 };
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -266,7 +266,7 @@ router.get("/github/:username", async (req, res) => {
     const options = {
       uri: `https://api.github.com/users/${
         req.params.username
-      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+      }/repos?per_page=4&sort=created:asc&client_id=${config.get(
         "githubClientID"
       )}&client_secret=${config.get("githubSecret")}`,
       method: "GET",
