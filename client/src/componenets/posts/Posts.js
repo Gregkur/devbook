4eb832d0d705d27eddeee6d2dae16a7e11fb435d
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-04-12 14:55:52 +0200

    finished single post item

--- client/src/componenets/posts/Posts.js
+++ client/src/componenets/posts/Posts.js
@@ -5,6 +5,7 @@ import { getPosts } from "../../actions/post";
 
 import Spinner from "../layout/Spinner";
 import PostItem from "./PostItem";
+import PostForm from "./PostForm";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
@@ -21,6 +22,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
       <p className="lead">
         <i className="fas fa-user" /> Welcome to the community
       </p>
+      <PostForm />
       <div className="posts">{mappedPosts}</div>
     </>
   );
